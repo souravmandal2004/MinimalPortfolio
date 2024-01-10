931339d50662b77651cab6751a9d8b47b7986346
souravmandal2004 <mandalsourav2004@gmail.com>
2024-01-10 19:33:26 +0530

    path fixed 1

--- src/Data/portfolio.js
+++ src/Data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: "Weather App",
-        imgUrl: "assets/WeatherApp.jpg",
+        imgUrl: "./public/assets/WeatherApp.jpg",
         stack: ["Html", "Css", "JavaScript"],
         link: "https://souravmandal2004.github.io/WeatherApp"
     },
     {
         title: "Password Generator",
-        imgUrl: "assets/PasswordGenerator.jpg",
+        imgUrl: "./public/assets/PasswordGenerator.jpg",
         stack: ["Html", "Css", "JavaScript"],
         link: "https://souravmandal2004.github.io/PasswordGenerator"
     },
     {
         title: "Share Modal",
-        imgUrl: "assets/shareModal.jpg",
+        imgUrl: "./public/assets/shareModal.jpg",
         stack: ["Html", "Css", "JavaScript"],
         link: "https://souravmandal2004.github.io/shareModal"
     },
