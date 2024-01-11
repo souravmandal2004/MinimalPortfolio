3607a48ca29520892e3c6cb14bcfcbe6b94ba0a7
Sourav Mandal <111380641+souravmandal2004@users.noreply.github.com>
2024-01-11 23:27:48 +0530

    Update portfolio.js
    Update paths of images

--- src/Data/portfolio.js
+++ src/Data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: "Weather App",
-        imgUrl: "./public/assets/WeatherApp.jpg",
+        imgUrl: "assets/WeatherApp.jpg",
         stack: ["Html", "Css", "JavaScript"],
         link: "https://souravmandal2004.github.io/WeatherApp"
     },
     {
         title: "Password Generator",
-        imgUrl: "./public/assets/PasswordGenerator.jpg",
+        imgUrl: "assets/PasswordGenerator.jpg",
         stack: ["Html", "Css", "JavaScript"],
         link: "https://souravmandal2004.github.io/PasswordGenerator"
     },
     {
         title: "Share Modal",
-        imgUrl: "./public/assets/shareModal.jpg",
+        imgUrl: "assets/shareModal.jpg",
         stack: ["Html", "Css", "JavaScript"],
         link: "https://souravmandal2004.github.io/shareModal"
     },
